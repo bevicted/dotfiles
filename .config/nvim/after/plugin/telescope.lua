3315563aabcda8421b7b74999ff0195784d1c84c
Bence Vidosits <bence.vidosits.dev@gmail.com>
2023-05-07 11:36:00 +0200

    search hidden files with telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,3 +2,16 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
+
+require("telescope").setup {
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+        live_grep = {
+            additional_args = function(opts)
+                return { "--hidden" }
+            end
+        },
+    },
+}
