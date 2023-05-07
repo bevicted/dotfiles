12ed3305f02c09c43e6f2663a9a44c99a1ac5467
Bence Vidosits <bence.vidosits.dev@gmail.com>
2023-05-07 11:24:52 +0200

    fix rg not using ripgrep

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,4 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>rg', function()
-	builtin.grep_string({ search = vim.fn.input("rg > ") });
-end)
-
+vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
