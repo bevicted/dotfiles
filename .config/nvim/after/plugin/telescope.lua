385f77559adbbe8a6a8c05d81c129c23719a9df7
Bence Vidosits <bence.vidosits.dev@gmail.com>
2023-05-07 13:27:03 +0200

    fix telescope poking around in .git/

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,11 @@ vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
 
 require("telescope").setup {
+    defaults = {
+        file_ignore_patterns = {
+            ".git/",
+        },
+    },
     pickers = {
         find_files = {
             hidden = true,
