f10232fbc52fce6a9e94e516c8c9dd492783c3f9
Bence Vidosits <bence.vidosits1@ibm.com>
2023-05-08 15:10:57 +0200

    fd, grep aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -4,6 +4,8 @@
 alias vim='nvim'
 alias ls='exa --long --classify --all --group-directories-first --binary --time-style=long-iso'
 alias cat='bat --paging=never'
+alias grep='rg'
+alias find='fd'
 
 # shortcuts
 alias ic='ibmcloud'
