b18cbcc1c4cf255bbfd23a9a728121cd1c8a5754
Bence Vidosits <bence.vidosits1@ibm.com>
2023-10-30 22:28:02 +0100

    add keymap for telescope keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -497,6 +497,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>rg', require('telescope.builtin').live_grep, { desc = '[r]ip[g]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -734,4 +735,3 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
