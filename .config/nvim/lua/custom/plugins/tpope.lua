2dd34683c4109c1fa900225ea7688ccd950c5854
Bence Vidosits <bence.vidosits1@ibm.com>
2024-03-22 21:42:02 +0100

    improve fugitive

--- .config/nvim/lua/custom/plugins/tpope.lua
+++ .config/nvim/lua/custom/plugins/tpope.lua
@@ -3,11 +3,19 @@ return {
     'tpope/vim-fugitive',
     keys = {
       { '<leader>vf', '<cmd>Git<cr>', desc = '[V]im [F]ugitive - Git' },
-      { '<leader>vb', '<cmd>Git blame<cr>', desc = '[V]im Fugitive - Git [B]lame' },
+      { '<leader>vb', '<cmd>Git blame -wCCC<cr>', desc = '[V]im Fugitive - Git [B]lame' },
+      { '<leader>vB', '<cmd>GBrowse<cr>', desc = '[V]im Fugitive - Git [B]rowse' },
     },
+    lazy = false,
   },
-  'tpope/vim-rhubarb',
-  'tpope/vim-sleuth',
+  {
+    'tpope/vim-rhubarb',
+    config = function()
+      vim.g.github_enterprise_urls = { 'https://github.ibm.com' }
+    end,
+    lazy = false,
+  },
+  { 'tpope/vim-sleuth', lazy = false },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
