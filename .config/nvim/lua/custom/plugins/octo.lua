129b631a185e72eb37ab12b640eb680bc9a7c777
Bence Vidosits <bence.vidosits1@ibm.com>
2024-03-25 10:06:31 +0100

    customize octo

--- .config/nvim/lua/custom/plugins/octo.lua
+++ .config/nvim/lua/custom/plugins/octo.lua
@@ -14,27 +14,42 @@ return {
   config = function()
     require('octo').setup {
       default_merge_method = 'squash',
+      mappings_disable_default = true,
     }
     local octo_cmds = require('octo.commands').commands
+
+    vim.keymap.set('n', '<leader>gha', octo_cmds.actions, { desc = '[G]it[H]ub [A]ctions' })
+    vim.keymap.set('n', '<leader>ghs', function()
+      octo_cmds.search('repo:' .. vim.g.squad_repo .. ' ' .. vim.fn.input 'Query: ')
+    end, { desc = '[G]it[H]ub [S]earch' })
 
     vim.keymap.set('n', '<leader>ghpc', octo_cmds.pr.create, { desc = '[G]it[H]ub [P]R [C]reate' })
     vim.keymap.set('n', '<leader>ghpC', octo_cmds.pr.close, { desc = '[G]it[H]ub [P]R [C]lose' })
     vim.keymap.set('n', '<leader>ghpo', octo_cmds.pr.checkout, { desc = '[G]it[H]ub [P]R check[O]ut' })
-    vim.keymap.set('n', '<leader>gha', octo_cmds.actions, { desc = '[G]it[H]ub [A]ctions' })
     vim.keymap.set('n', '<leader>ghpl', octo_cmds.pr.list, { desc = '[G]it[H]ub [P]R [L]ist' })
     vim.keymap.set('n', '<leader>ghpe', function()
       octo_cmds.pr.edit(vim.fn.input 'PR: ')
     end, { desc = '[G]it[H]ub [P]R [E]dit' })
 
-    vim.keymap.set('n', '<leader>ghic', octo_cmds.issue.create, { desc = '[G]it[H]ub [I]ssue [C]reate' })
-    vim.keymap.set('n', '<leader>ghil', octo_cmds.issue.list, { desc = '[G]it[H]ub [I]ssue [L]ist' })
+    vim.keymap.set('n', '<leader>ghic', function()
+      octo_cmds.issue.create(vim.g.squad_repo)
+    end, { desc = '[G]it[H]ub [I]ssue [C]reate' })
+    vim.keymap.set('n', '<leader>ghil', function()
+      octo_cmds.issue.list(vim.g.squad_repo)
+    end, { desc = '[G]it[H]ub [I]ssue [L]ist' })
     vim.keymap.set('n', '<leader>ghie', function()
-      octo_cmds.issue.edit(vim.fn.input 'Issue: ')
+      octo_cmds.issue.edit(vim.g.squad_repo, vim.fn.input 'Issue: ')
     end, { desc = '[G]it[H]ub [I]ssue [E]dit' })
     vim.keymap.set('n', '<leader>ghiC', octo_cmds.issue.close, { desc = '[G]it[H]ub [I]ssue [C]lose' })
     vim.keymap.set('n', '<leader>ghiR', octo_cmds.issue.reopen, { desc = '[G]it[H]ub [I]ssue [R]eopen' })
     vim.keymap.set('n', '<leader>ghiy', octo_cmds.issue.url, { desc = '[G]it[H]ub [I]ssue [Y]ank URL' })
     vim.keymap.set('n', '<leader>ghib', octo_cmds.issue.browser, { desc = '[G]it[H]ub [I]ssue Open In [B]rowser' })
+
+    vim.keymap.set('n', '<leader>ghca', octo_cmds.comment.add, { desc = '[G]it[H]ub [C]omment [A]dd' })
+    vim.keymap.set('n', '<leader>ghcd', octo_cmds.comment.delete, { desc = '[G]it[H]ub [C]omment [D]elete' })
+
+    vim.keymap.set('n', '<leader>ghla', octo_cmds.label.add, { desc = '[G]it[H]ub [L]abel [A]dd' })
+    vim.keymap.set('n', '<leader>ghlr', octo_cmds.label.remove, { desc = '[G]it[H]ub [L]abel [R]remove' })
   end,
 }
 
