98ac760a7029ebbb3d91ff3acfe06f4bf74394c9
Bence Vidosits <bence.vidosits1@ibm.com>
2024-03-25 08:13:07 +0100

    add desc to undotree

--- .config/nvim/lua/custom/plugins/undotree.lua
+++ .config/nvim/lua/custom/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   {
     'mbbill/undotree',
     keys = {
-      { '<leader>u', vim.cmd.UndotreeToggle },
+      { '<leader>u', vim.cmd.UndotreeToggle, desc = '[U]ndotree' },
     },
     config = function()
       vim.g.undotree_SetFocusWhenToggle = 1
