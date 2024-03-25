53511213c9dbe8d8ec7501bcdaf587c869136fbd
Bence Vidosits <bence.vidosits1@ibm.com>
2024-03-25 10:59:58 +0100

    add navbuddy
    Signed-off-by: Bence Vidosits <bence.vidosits1@ibm.com>

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -14,6 +14,16 @@ return {
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
       { 'folke/neodev.nvim', opts = {} },
+
+      {
+        'SmiteshP/nvim-navbuddy',
+        dependencies = {
+          'SmiteshP/nvim-navic',
+          'MunifTanjim/nui.nvim',
+        },
+        keys = { { '<leader>n', '<cmd>Navbuddy<cr>' } },
+        opts = { lsp = { auto_attach = true } },
+      },
     },
     config = function()
       -- Brief aside: **What is LSP?**
