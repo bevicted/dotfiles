6c99b7a1facdf5b4d088be41da0799db5029d91a
bevicted <38434845+bvidosits@users.noreply.github.com>
2024-04-06 18:39:32 +0200

    wisen aliases up

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,11 +1,13 @@
 #!/usr/bin/env zsh
 
 # tool replacements
-alias vim='nvim'
-alias ls='exa --long --classify --all --group-directories-first --binary --time-style=long-iso'
-alias cat='bat --paging=never'
-alias grep='rg'
-alias find='fd'
+alias_command_if_exists 'vim' 'nvim'
+alias_command_if_exists 'ls' 'exa --long --classify --all --group-directories-first --binary --time-style=long-iso'
+alias_command_if_exists 'cat' 'bat --paging=never'
+alias_command_if_exists 'cat' 'batcat --paging=never'
+alias_command_if_exists 'grep' 'rg'
+alias_command_if_exists 'find' 'fd'
+alias_command_if_exists 'find' 'fdfind'
 
 # shortcuts
 alias ic='ibmcloud'
