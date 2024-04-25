b4ceabd06e9251919c502091986ed974b3b37286
Bence Vidosits <bence.vidosits1@ibm.com>
2024-04-25 10:09:34 +0200

    add gitsigns keys
    Signed-off-by: Bence Vidosits <bence.vidosits1@ibm.com>

--- .config/nvim/lua/kickstart/plugins/which_key.lua
+++ .config/nvim/lua/kickstart/plugins/which_key.lua
@@ -27,7 +27,13 @@ return {
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>v'] = { name = '[V]im Fugitive', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       }
+
+      -- visual mode
+      require('which-key').register({
+        ['<leader>h'] = { 'Git [H]unk' },
+      }, { mode = 'v' })
     end,
   },
 }
