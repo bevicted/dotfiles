6bef5cf07408e568ef5ea26e4910fbee01c95f14
Bisons Evicted <170766675+bevicted@users.noreply.github.com>
2024-08-29 07:13:53 +0200

    arken: keep history

--- .mozilla/firefox/user.arkenfox/user-overrides.js
+++ .mozilla/firefox/user.arkenfox/user-overrides.js
@@ -5,5 +5,6 @@ user_pref("media.eme.enabled", true); // 2022
 user_pref("browser.startup.page", 3); // 0102
   // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
   // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
-user_pref("privacy.clearOnShutdown.history", false); // 2811
+user_pref("privacy.clearOnShutdown.history", false); // 2811 DEPRECATED
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
   // user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
