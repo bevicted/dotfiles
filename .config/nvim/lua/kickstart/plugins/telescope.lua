bceede8c661d231f27d91376f1428ef95d3fc074
bevicted <170766675+bevicted@users.noreply.github.com>
2024-08-30 09:19:29 +0200

    telescope: ignore .godot/
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- .config/nvim/lua/kickstart/plugins/telescope.lua
+++ .config/nvim/lua/kickstart/plugins/telescope.lua
@@ -61,6 +61,7 @@ return {
             '.git/',
             'node_modules/',
             '.venv/',
+            '.godot/',
           },
           -- layout_strategy = "vertical",
           -- layout_config = {
