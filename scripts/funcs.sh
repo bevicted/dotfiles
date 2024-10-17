d7b276d5fd93236514bdfcb1c4f03935eab8bc84
bevicted <170766675+bevicted@users.noreply.github.com>
2024-10-17 07:20:33 +0200

    personal git aliases
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- scripts/funcs.sh
+++ scripts/funcs.sh
@@ -1,5 +1,45 @@
 #!/usr/bin/env bash
 
+command_exists() {
+    command -v "$1" &> /dev/null
+    return $?
+}
+
+alias_command_if_exists() {
+  if command_exists "${2%% *}"
+  then
+    alias "$1"="$2"
+  fi
+}
+
+git_main_branch () {
+    command git rev-parse --git-dir &> /dev/null || return
+    local ref
+    for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}
+    do
+        if command git show-ref -q --verify $ref
+        then
+            echo ${ref:t}
+            return 0
+        fi
+    done
+    echo master
+    return 1
+}
+
+gbmv () {
+        if [[ -z "$1" || -z "$2" ]]
+        then
+                echo "Usage: $0 old_branch new_branch"
+                return 1
+        fi
+        git branch -m "$1" "$2"
+        if git push origin :"$1"
+        then
+                git push --set-upstream origin "$2"
+        fi
+}
+
 get_subcommands() {
     help_text=$("$@" --help)
     if [ -n "$(echo "${help_text}" | rg '^COMMANDS:$')" ]; then
