f750af6ddf3806396119d91b1f8aa8c29b8c0238
Bence Vidosits <bence.vidosits1@ibm.com>
2024-10-17 07:25:35 +0200

    fix line number spacing
    Signed-off-by: Bence Vidosits <bence.vidosits1@ibm.com>

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -13,7 +13,7 @@ vim.opt.guicursor = ''
 vim.wo.number = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.o.statuscolumn = '%s %l %r'
+vim.o.statuscolumn = '%s %l %r '
 
 -- [ indent ]
 
