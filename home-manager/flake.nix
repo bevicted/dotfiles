fa8e8220d9da35b1d3cab0017057525683c9b0f1
bevicted <170766675+bevicted@users.noreply.github.com>
2024-10-19 22:53:19 +0200

    hm: ping terraform version
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,16 +4,21 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs_terraform.url = "github:nixos/nixpkgs/517501bcf14ae6ec47efd6a17dda0ca8e6d866f9";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, nixpkgs_terraform, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs_terraform = nixpkgs_terraform.legacyPackages.${system};
+      terraform_overlay = final: prev: {
+        terraform = pkgs_terraform.terraform;
+      };
+      pkgs = nixpkgs.legacyPackages.${system}.extend terraform_overlay;
     in {
       homeConfigurations."bevicted" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
