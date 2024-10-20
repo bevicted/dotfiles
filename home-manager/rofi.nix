34f31b51d1e204fe9c1cf2f4a3f4d2d5824bcfe4
bevicted <170766675+bevicted@users.noreply.github.com>
2024-10-20 20:35:12 +0200

    hm: rofi fix

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -2,20 +2,11 @@
 {
   programs.rofi = {
     enable = true;
-    theme = "arc-dark";
+    theme = "Arc-Dark";
     extraConfig = {
       modi = "drun,run";
       show-icons = true;
       matching = "fuzzy";
-      # TODO: this doesn't exist here, maybe I don't even need it though
-      # timeout = {
-      #   action = "kb-cancel";
-      #   delay =  0;
-      # };
-      # filebrowser = {
-      #   directories-first = true;
-      #   sorting-method =    "name";
-      # };
     };
   };
 }
