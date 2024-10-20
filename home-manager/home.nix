ed531f0bd66f5fe5125f83e0172c263325d67f63
bevicted <170766675+bevicted@users.noreply.github.com>
2024-10-20 20:57:02 +0200

    hm: add nerffont JetBrainsMono

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -51,11 +51,8 @@
     sampler
     hey
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # fonts
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
