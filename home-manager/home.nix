a3f5b2d96391d50b3adadfc1339ee2515ff9e33e
Bence Vidosits <bence.vidosits1@ibm.com>
2024-11-06 14:42:23 +0100

    WIP: ff
    Signed-off-by: Bence Vidosits <bence.vidosits1@ibm.com>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,11 +16,6 @@
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
   imports = [
-    # TODO: remove when https://github.com/nix-community/home-manager/pull/5355 gets merged:
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
-      sha256 = "1krclaga358k3swz2n5wbni1b2r7mcxdzr6d7im6b66w3sbpvnb3";
-    })
     ./alacritty.nix
     ./gitui.nix
     ./nix.nix
