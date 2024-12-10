b13039f39f30d853689c5d09ee11fe2d2387b996
Bence Vidosits <bence.vidosits1@ibm.com>
2024-12-10 10:21:31 +0100

    funcs: add get_alias
    Signed-off-by: Bence Vidosits <bence.vidosits1@ibm.com>

--- scripts/funcs.sh
+++ scripts/funcs.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
 command_exists() {
-    command -v "$1" &> /dev/null
+    command -v "$1" &>/dev/null
     return $?
 }
 
 alias_command_if_exists() {
-  if command_exists "${2%% *}"
-  then
-    alias "$1"="$2"
-  fi
+    if command_exists "${2%% *}"; then
+        alias "$1"="$2"
+    fi
 }
 
-git_main_branch () {
-    command git rev-parse --git-dir &> /dev/null || return
+git_main_branch() {
+    command git rev-parse --git-dir &>/dev/null || return
     local ref
-    for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}
-    do
-        if command git show-ref -q --verify $ref
-        then
-            echo ${ref:t}
+    for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
+        if command git show-ref -q --verify "${ref}"; then
+            echo "${ref:t}"
             return 0
         fi
     done
@@ -27,17 +24,15 @@ git_main_branch () {
     return 1
 }
 
-gbmv () {
-        if [[ -z "$1" || -z "$2" ]]
-        then
-                echo "Usage: $0 old_branch new_branch"
-                return 1
-        fi
-        git branch -m "$1" "$2"
-        if git push origin :"$1"
-        then
-                git push --set-upstream origin "$2"
-        fi
+gbmv() {
+    if [[ -z "$1" || -z "$2" ]]; then
+        echo "Usage: $0 old_branch new_branch"
+        return 1
+    fi
+    git branch -m "$1" "$2"
+    if git push origin :"$1"; then
+        git push --set-upstream origin "$2"
+    fi
 }
 
 get_subcommands() {
@@ -71,4 +66,8 @@ map_command() {
         IFS=$'\n'
         echo "${target_commands[*]}" | sort
     )"
+}
+
+get_alias() {
+    alias "$1" | cut -d\' -f2
 }
