bfe6f8ac041b4ca60f76bba19c4398bb4c7c920a
bevicted <170766675+bevicted@users.noreply.github.com>
2024-12-15 22:11:57 +0100

    WIP: nvim: restructure
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   {
     'mbbill/undotree',
     keys = {
-      { '<leader>u', vim.cmd.UndotreeToggle, desc = '[U]ndotree' },
+      { '<leader>u', vim.cmd.UndotreeToggle, desc = '[u]ndotree' },
     },
     config = function()
       vim.g.undotree_SetFocusWhenToggle = 1
