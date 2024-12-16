0f6728cd03701f2fdfe4860b8f3ec0664456c4a3
bevicted <170766675+bevicted@users.noreply.github.com>
2024-12-16 11:27:39 +0100

    nvim: tokyonight -> catpuccin-mocha
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    enabled = true,
+    lazy = false,
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
