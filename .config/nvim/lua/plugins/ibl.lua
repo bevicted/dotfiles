89df837f177aa840d551223cda2571b562694202
bevicted <170766675+bevicted@users.noreply.github.com>
2024-12-20 11:26:12 +0100

    nvim: revert ibl
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- .config/nvim/lua/plugins/ibl.lua
+++ .config/nvim/lua/plugins/ibl.lua
@@ -3,20 +3,7 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     opts = {},
-    config = function()
-      local highlight = {
-        'CursorColumn',
-        'Whitespace',
-      }
-
-      require('ibl').setup {
-        indent = { highlight = highlight, char = '' },
-        whitespace = {
-          highlight = highlight,
-          remove_blankline_trail = false,
-        },
-        scope = { enabled = false },
-      }
-    end,
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
