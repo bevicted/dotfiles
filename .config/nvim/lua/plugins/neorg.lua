c79c06b447b34e1b5a3b943703f0aaa5de449fca
bevicted <170766675+bevicted@users.noreply.github.com>
2025-09-21 16:31:27 +0200

    config neorg
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -2,5 +2,20 @@ return {
   'nvim-neorg/neorg',
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = '*',
-  config = true,
+  config = function()
+    require('neorg').setup {
+      load = {
+        ['core.defaults'] = {},
+        ['core.concealer'] = {},
+        ['core.dirman'] = {
+          config = {
+            workspaces = {
+              notes = '~/dev/notes',
+            },
+            index = 'index.norg',
+          },
+        },
+      },
+    }
+  end,
 }
