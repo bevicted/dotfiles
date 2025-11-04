875e1b09fb0c6af7615bf5c357fd91519f611b26
bevicted <170766675+bevicted@users.noreply.github.com>
2025-11-04 14:25:07 +0100

    osx fixes

--- obsidian_vault/Shells/zsh/zsh.md
+++
@@ -1,3 +0,0 @@
-## keybinds
-
-Keybinds are controlled by [[Shells/zsh/zshzle|zshzle]]
--- python/setup.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Shim for pip's editable installation."""
-
-from setuptools import setup
-
-setup()
