c828dab8c55ba3fc0760ca0697edf7716a6d03ce
bevicted <170766675+bevicted@users.noreply.github.com>
2026-04-07 10:27:53 +0200

    fix highlighting
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,27 +4,46 @@ return {
     branch = 'main',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter').setup {
-        ensure_installed = {
-          'c',
-          'go',
-          'lua',
-          'markdown',
-          'markdown_inline',
-          'query',
-          'vim',
-          'vimdoc',
-        },
+      local ts = require 'nvim-treesitter'
+      ts.setup()
+
+      local wanted = {
+        'bash',
+        'c',
+        'go',
+        'gomod',
+        'gosum',
+        'json',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'python',
+        'query',
+        'rust',
+        'toml',
+        'vim',
+        'vimdoc',
+        'yaml',
       }
 
-      -- Disable treesitter highlighting for large files
+      local installed = ts.get_installed()
+      local to_install = vim.tbl_filter(function(lang)
+        return not vim.list_contains(installed, lang)
+      end, wanted)
+
+      if #to_install > 0 then
+        ts.install(to_install)
+      end
+
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
           local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
           if ok and stats and stats.size > max_filesize then
             vim.treesitter.stop(args.buf)
+            return
           end
+          pcall(vim.treesitter.start, args.buf)
         end,
       })
     end,
